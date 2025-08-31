eb2c8f81897d6ce6be04990591c5484e784a0ae6
VISHAL RAM PATEL <vip097348@gmail.com>
2025-08-31 14:03:15 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+String fname = "john";
+String lname = "Deo";
+int age = 24;
+
+  public static void main(String[] args) {
+   Main myobj = new Main();
+    
+   System.out.println("Name  " + myobj.fname + "" + myobj.lname);
+   
+   System.out.println("Age: " + myobj.age);
+ 
+ 
+  }
+}
+    
+
+
